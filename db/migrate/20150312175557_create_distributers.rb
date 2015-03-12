ae300f1bb6fc030ffc4179dd79117ff11c438364
Harjinder <hsingh@enbake.com>
2015-03-13 00:04:11 +0530

    minor change

--- db/migrate/20150312175557_create_distributers.rb
+++ db/migrate/20150312175557_create_distributers.rb
@@ -2,7 +2,7 @@ class CreateDistributers < ActiveRecord::Migration
   def change
     create_table :distributers do |t|
       t.string :email
-      t.boolean :approve
+      t.boolean :approve, :default => false
       t.string :first_name
       t.string :last_name
       t.text :address
