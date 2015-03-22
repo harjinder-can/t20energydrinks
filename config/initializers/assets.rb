d849a7a01001801c17adf7a9e3700b772d9f7e7b
Sahil <sahil@sahil-Vostro-2520.(none)>
2015-03-22 14:44:37 +0530

    #upload feature

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(application.js application.css active_admin.css.scss active_admin.js.coffee bootstrap.css bootstrap.js )
+Rails.application.config.assets.precompile += %w(application.js application.css active_admin.css.scss active_admin.js.coffee bootstrap.css bootstrap.js camera.css component.css font-awesome.css form.css grid.css ie1.css menu.css reset.css style.css touchTouch.css html5shiv.js jquery.js jquery.tabs.min.js jquery.ui.totop.js jquery-migrate-1.1.1.js main.js camera.js forms.js jquery.equalheights.js jquery.easing.1.3.js jquery.mobile.customized.min.js map.js snap.svg-min.js TMForm.js list.css touchTouch.jquery.js  jquery.mobile.customized.min.js) 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312175609) do
+ActiveRecord::Schema.define(version: 20150322082520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,19 @@ ActiveRecord::Schema.define(version: 20150312175609) do
     t.string   "first_name"
     t.string   "last_name"
     t.text     "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notifications", force: true do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", force: true do |t|
+    t.string   "avatar"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
